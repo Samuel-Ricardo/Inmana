59f4320f73ab13ab5e834bb093e8808219d94c3d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-20 17:26:03 -0300

    Created: ErrorView

--- Inmana/lib/inmana_web/views/error_view.ex
+++ Inmana/lib/inmana_web/views/error_view.ex
@@ -1,6 +1,8 @@
 defmodule InmanaWeb.ErrorView do
   use InmanaWeb, :view
 
+  alias Ecto.Changeset
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -13,4 +15,21 @@ defmodule InmanaWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  def render("error.json", %{result: %Changeset{} = changeset}) do
+
+    %{message: translate_erros(changeset)}
+  end
+
+  defp translate_erros(changeset) do
+
+    Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+
+
+  end
+
 end
