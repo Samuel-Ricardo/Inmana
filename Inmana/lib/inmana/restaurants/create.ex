fbd8782d459d8dbf656ca60c42d434bcdbd0a84f
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-20 15:20:52 -0300

    Create.ex

--- Inmana/lib/inmana/restaurants/create.ex
+++ Inmana/lib/inmana/restaurants/create.ex
@@ -1,3 +1,16 @@
 defmodule Inmana.Restaurants.Create do
+  alias Inmana.{Repo, Restaurant}
+
+  def call(params) do
+
+    params
+     |> Restaurant.changeset()
+     |> Repo.insert()
+     |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Restaurant{}} = result), do: result
+
+  defp handle_insert({:error, result}), do: {:error, %{result: result, status: :bad_request}}
 
 end
