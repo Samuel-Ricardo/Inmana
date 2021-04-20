9dc6ec1a283763658b489d5081e3c788a2653269
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-20 14:37:04 -0300

    refactor

--- Inmana/lib/inmana_web/controllers/welcome_controller.ex
+++ Inmana/lib/inmana_web/controllers/welcome_controller.ex
@@ -10,18 +10,15 @@ defmodule InmanaWeb.WelcomeController do
      |> handle_response(connection)
   end
 
-  defp handle_response({:ok, message}, conn) do
+  defp handle_response({:ok, message}, connection), do: render_response(connection, message, :ok)
 
-    conn
-     |> put_status(:ok)
+  defp handle_response({:error, message}, connection), do: render_response(connection, message, :bad_request)
+
+  defp render_response(connection, message, status) do
+
+    connection
+     |> put_status(status)
      |> json(%{message: message})
 
-  end
-
-  defp handle_response({:error, message}, conn) do
-
-    conn
-     |> put_status(:bad_request)
-     |> json(%{message: message})
   end
 end
