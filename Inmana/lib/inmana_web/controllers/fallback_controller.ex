1866bec1ff536baa7ad9c4a3a5d14c760c4c1eb1
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-20 17:16:17 -0300

    Created: FallbackController

--- Inmana/lib/inmana_web/controllers/fallback_controller.ex
+++ Inmana/lib/inmana_web/controllers/fallback_controller.ex
@@ -1,4 +1,13 @@
 defmodule InmanaWeb.FallbackController  do
   use InmanaWeb, :controller
 
+  alias InmanaWeb.ErrorView
+
+  def call(connection, {:error, %{result: result, status: status}}) do
+
+    connection
+     |> put_status(status)
+     |> put_view(ErrorView)
+     |> render("error.json", result: result)
+  end
 end
