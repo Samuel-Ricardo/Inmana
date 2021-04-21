e6dfb5d809ac3dedc8504bfe43793e42d7d99d3e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 08:39:43 -0300

    Create: Schema - supplies

--- Inmana/lib/inmana/supply.ex
+++ Inmana/lib/inmana/supply.ex
@@ -2,4 +2,18 @@ defmodule Inmana.Supply do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Inmana.Restaurant
+
+  
+
+  schema "supplies" do
+    field :description, :string
+    field :expiration_date, :date
+    field :responsible, :string
+
+    belongs_to :restaurant, Restaurant
+
+    timestamps()
+  end
+
 end
