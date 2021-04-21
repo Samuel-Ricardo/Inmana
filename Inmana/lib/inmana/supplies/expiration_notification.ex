00c95dbd05f9c986f4b24ab6188d1943daa959b2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 12:23:21 -0300

    create module

--- Inmana/lib/inmana/supplies/expiration_notification.ex
+++ Inmana/lib/inmana/supplies/expiration_notification.ex
@@ -1,3 +1,16 @@
 defmodule Inmana.Supplies.ExpirationNotification do
-  
+  alias Inmana.Mailer
+  alias Inmana.Sipplies.{ExpirationEmail, GetByExpiration}
+
+  def send do
+
+    data = GetByExpiration.call()
+
+    Enum.each(data, fn {to_email, supplies} ->
+      to_email
+       |> ExpirationEmail.create(supplies)
+       |> Mailer.deliver_later!()
+    end)
+
+  end
 end
