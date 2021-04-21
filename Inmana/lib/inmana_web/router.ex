b8bf52f2ee76b43f03bcc29ed56228bbdff94862
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 10:53:12 -0300

    refactor: routes - supply

--- Inmana/lib/inmana_web/router.ex
+++ Inmana/lib/inmana_web/router.ex
@@ -12,7 +12,7 @@ defmodule InmanaWeb.Router do
 
     post "/restaurants", RestaurantsController, :create
 
-    post "/supplies", SuppliesController, :create
+    resources "/supplies", SuppliesController, only: [:create, :show]
 
   end
 
