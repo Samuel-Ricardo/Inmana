336a10deb1935823061797aa856778b20264c6d9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 11:48:04 -0300

    query

--- Inmana/lib/inmana/supplies/get_by_expiration.ex
+++ Inmana/lib/inmana/supplies/get_by_expiration.ex
@@ -1,4 +1,21 @@
 defmodule Inmana.Supplies.GetByExpirationDate do
+  import Ecto.Query
 
-  
+  alias Inmana.Supply
+
+  def call do
+
+    today = Date.utc_today()
+
+    beginning_of_week = Date.beginnig_of_week(today)
+  end_of_week = Date.end_of_week(today)
+
+  query =
+    from supply in Supply,
+      where:
+        supply.expiration_date >= ^beginning_of_week and supply.expiration_date <= ^end_of_week,
+        preload: [:restaurant]
+
+  end
+
 end
