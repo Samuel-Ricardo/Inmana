59744ba89058e04afcc2d52407bccf67185210b6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 08:51:52 -0300

    Supply creater

---
+++ Inmana/lib/inmana/supplies/create.ex
@@ -0,0 +1,16 @@
+defmodule Inmana.Supplies.Create do
+  alias Inmana.{Repo, Supply}
+
+  def call(params) do
+
+    params
+     |> Supply.changeset()
+     |> Repo.insert()
+     |> handle_insert
+  end
+
+  defp handle_insert({:ok, %Supply{}} = result), do: result
+
+  defp handle_insert({:error, result}), do: {:error, %{result: result, status: :bad_request}}
+
+end
