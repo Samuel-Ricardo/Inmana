b25291af90b2760ba21e055fe7017fd83a57cb03
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 09:04:31 -0300

    Created: Controller Supplies

--- Inmana/lib/inmana_web/controllers/supplies_controller.ex
+++ Inmana/lib/inmana_web/controllers/supplies_controller.ex
@@ -1,4 +1,17 @@
 defmodule InmanaWeb.SuppliesController do
   use InmanaWeb, :controller
 
+  alias Inmana.Supply
+  alias InmanaWeb.FallbackController
+
+  action_fallback FallbackController
+
+  def create(connection, params) do
+    with {:ok, %Supply{} = supply} <- Inmana.create_supply(params) do
+      connection
+       |> put_status(:created)
+       |> render("create.json", supply: supply)
+    end
+  end
+
 end
