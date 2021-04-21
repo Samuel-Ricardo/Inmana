ebb8643c2e696d8481b0376a7a1758d733a8f3c8
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 12:41:36 -0300

    create email

--- Inmana/lib/inmana/supplies/expiration_email.ex
+++ Inmana/lib/inmana/supplies/expiration_email.ex
@@ -1,3 +1,17 @@
 defmodule Inamana.Supplies.ExpirationEmail do
-  
+  import Bamboo.Email
+
+  def create(to_email, supplies) do
+
+    new_email(
+      to: to_email,
+      from: "app@inmana.com.br",
+      subject: "Supplies that are abot to expire",
+      text_body: render_email_text(supplies)
+    )
+  end
+
+  def render_email_text(supplies) do
+
+  end
 end
