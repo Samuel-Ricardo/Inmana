85d3ee26d14cc36a41a7b2285d67dbef0199fb83
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-21 08:55:06 -0300

    defdelegate

--- Inmana/lib/inmana.ex
+++ Inmana/lib/inmana.ex
@@ -1,6 +1,10 @@
 defmodule Inmana do
-  alias Inmana.Restaurants.Create
+  alias Inmana.Restaurants.Create, as: Create_Restaurant
+  alias Inmana.Supplies.Create, as: Create_Supply
 
-  defdelegate create_restaurant(params), to: Create, as: :call
+
+  defdelegate create_restaurant(params), to: Create_Restaurant, as: :call
+
+  defdelegate create_supply(params), to: Create_Supply, as: :call
 
 end
