ec448f004c1385fb6ba762cbfa1e7f621dc4e5c0
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-23 13:34:55 -0300

    succes test - ( InmanaWeb.RestaurantsController)

--- Inmana/test/inmana_web/controllers/restaurants_controller_test.exs
+++ Inmana/test/inmana_web/controllers/restaurants_controller_test.exs
@@ -1,4 +1,26 @@
-defmodule  do
+defmodule InmanaWeb.RestaurantsControllerTest do
+  use InmanaWeb.ConnCase, async: true
 
-  
+  describe "create/2" do
+
+    test "WHen all params are valid< create the user", %{conn: conn} do
+      params = %{name: "Siri Cascudo", email: "siri@fenda.com"}
+
+      response =
+        conn
+         |> post(Routes.restaurants_path(conn, :create, params))
+         |> json_response(:ok)
+
+         assert %{
+          "message" => "Restaurant created",
+          "restaurant" => %{
+            "email" => "siri@fenda.com",
+            "id" => _id,
+            "name" => "Siri Cascudo"
+          }
+        } = response
+    end
+    
+  end
+
 end
