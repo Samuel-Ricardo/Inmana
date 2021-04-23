a01edce88f0582c903748dae006372245bfd4eef
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-23 14:30:06 -0300

    render test

--- Inmana/test/inmana_web/views/restaurants_view_test.exs
+++ Inmana/test/inmana_web/views/restaurants_view_test.exs
@@ -1,3 +1,28 @@
-defmodule InmanaWeb.RestaurantsView do
-  
+defmodule InmanaWeb.RestaurantsViewTest do
+  use InmanaWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Inmana.Restaurant
+  alias InmanaWeb.RestaurantsView
+
+  describe "render/2" do
+    test "renders create.json" do
+      params = %{name: "Siri Cascudo", email: "siri@fenda.com"}
+      {:ok, restaurant} = Inmana.create_restaurant(params)
+
+      response = render(RestaurantsView, "create.json", restaurant: restaurant)
+
+      assert %{
+        message: "Restaurant created",
+        restaurant: %Inmana.Restaurant{
+          email: "siri@fenda.com",
+          id: _id,
+          name: "Siri Cascudo",
+        }
+      } = response
+
+
+    end
+  end
 end
