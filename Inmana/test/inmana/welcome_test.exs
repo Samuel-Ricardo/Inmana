a123af9db9613265d7e80c9e3b61d4e1e80a4e6c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-23 12:37:20 -0300

    Created: Test - welcome/1

--- Inmana/test/inmana/welcome_test.exs
+++ Inmana/test/inmana/welcome_test.exs
@@ -2,4 +2,14 @@ defmodule Inmana.WelcomerTest do
   use ExUnit.Case
 
   alias Inmana.Welcomer
+
+  describe "welcome/1" do
+    test "When the user is speciel returns a special message"
+      params = %{"name" => "pedro >()", "age" => "18"}
+      expected_result = {:ok, "You are very special Pedro >()"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+  end
 end
