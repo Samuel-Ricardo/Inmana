e475622f8d6ce359461281b2c1e3eebf6190746b
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-23 12:55:27 -0300

    tests

--- Inmana/test/inmana/welcome_test.exs
+++ Inmana/test/inmana/welcome_test.exs
@@ -4,12 +4,32 @@ defmodule Inmana.WelcomerTest do
   alias Inmana.Welcomer
 
   describe "welcome/1" do
-    test "When the user is speciel returns a special message"
+
+    test "When the user is special returns a special message" do
       params = %{"name" => "pedro >()", "age" => "18"}
-      expected_result = {:ok, "You are very special Pedro >()"}
+      expected_result = {:ok, "You are a very Special Pedro :()"}
 
       result = Welcomer.welcome(params)
 
       assert result == expected_result
+    end
+
+    test "When the user isn't special returns a special message" do
+      params = %{"name" => "pedro >:)", "age" => "20"}
+      expected_result = {:ok, "PEDRO ADULTO <:()"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
+
+    test "When the user is under age returns a message" do
+      params = %{"name" => "pedro >-( )", "age" => "16"}
+      expected_result = {:ok, "PEDRO ADULTO <:()"}
+
+      result = Welcomer.welcome(params)
+
+      assert result == expected_result
+    end
   end
 end
