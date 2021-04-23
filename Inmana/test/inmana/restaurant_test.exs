f6a45f9b70bb35afb9f809e5d72043b0fcaa32b1
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-04-23 13:06:09 -0300

    succes test

--- Inmana/test/inmana/restaurant_test.exs
+++ Inmana/test/inmana/restaurant_test.exs
@@ -1,3 +1,25 @@
 defmodule Inmana.RestaurantTest do
+  use Inmana.DataCase
 
+  alias Inmana.Restaurant
+
+  describe "changeset/1" do
+
+    test "When all params are valid return a valid changeset" do
+
+      params = %{name: "Siri Cascudo", email: "siri@fenda.com"}
+
+      response = Restaurant.changeset(params)
+
+      asset %Changeset{
+              changes: %{
+                email: "siri@fenda.com",
+                name: "Siri Cascudo"
+              },
+              valid?: true
+            } = response
+
+    end
+
+  end
 end
